72430cf51b36e7605b7d528ee5d5ae4f96ec227f
Max Maischein <corion@corion.net>
2011-02-14 21:51:37 +0100

    Add --sep and --uri to specify separators and uris

--- bin/scrape.pl
+++ bin/scrape.pl
@@ -8,14 +8,17 @@ use Pod::Usage;
 use Data::Dumper;
 
 GetOptions(
-    'uri' => \my $make_uri,
+    'uri:s' => \my @make_uri,
+    'sep:s' => \my $sep,
 ) or pod2usage(2);
 
+# make_uri can be a comma-separated list of columns to map
+# The index starts at one
+my %make_uri = map{ $_-1 => 1 } map{ split /,/ } @make_uri;
+$sep ||= "\t";
+
 # Now determine where we get the HTML to scrape from:
-my ($url,$selector,$nodepart) = @ARGV;
-if ($selector !~ m!^/!) {
-    $selector = selector_to_xpath( $selector );
-};
+my $url = shift @ARGV;
 
 my $html;
 if ($url eq '-') {
@@ -27,27 +30,44 @@ if ($url eq '-') {
 };
 
 # Now munge the XPath expression to select only the nodes we really want
-$nodepart ||= 'text';
-if ($nodepart =~ /^\@/) {
-    $selector = "$selector/$nodepart";
-};
+#$nodepart ||= 'text';
+#if ($nodepart =~ /^\@/) {
+#    $selector = "$selector/$nodepart";
+#};
 
 my $tree = HTML::TreeBuilder::XPath->new;
 $tree->parse($html);
 $tree->eof;
 
-my @nodes;
-if ($nodepart eq 'text') {
-    @nodes = map { join " ", @{ $_->{_content} } } $tree->findnodes($selector);
-} else {
-    @nodes = $tree->findvalues($selector);
+# now fetch all "rows"
+
+my @rows;
+
+my $rowidx=0;
+for my $selector (@ARGV) {
+    if ($selector !~ m!^/!) {
+        $selector = selector_to_xpath( $selector );
+    };
+    my @nodes;
+    if ($selector !~ m!/\@\w+$!) {
+        @nodes = map { $_->as_trimmed_text } $tree->findnodes($selector);
+    } else {
+        @nodes = $tree->findvalues($selector);
+    };
+    
+    if ($make_uri{ $rowidx }) {
+        @nodes = map { URI->new_abs( $_, $url )->as_string } @nodes;
+    };
+    
+    $rows[ $rowidx++ ] = \@nodes;
 };
 
-for (@nodes) {
-    if ($make_uri) {
-        $_ = URI->new_abs( $_, $url )->as_string;
-    };
-    print $_;
+for my $idx (0.. $#{ $rows[0] }) {
+    print join $sep, map { 
+            $rows[$_]->[$idx]
+        } 0..$#rows;
+    
     print "\n";
 };
+
 $tree->delete;
